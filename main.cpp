f65507674824bdcda7c3982e8b31e3f5c743ada5
voyadger01 <voyadgerkx@gmail.com>
2026-03-02 19:50:07 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -5,61 +5,55 @@
 // Неявный интерфейс для Т: T(), T(const T&), T(T&&)
 // Неявный интерфейс для F: operator(), F(const F&)
 
-template < class T >
-struct BiList
+template < class T > struct BiList
 {
   T val;
-  BiList< T >* next;
-  BiList< T >* prev;
+  BiList< T > *next;
+  BiList< T > *prev;
 };
 
-template < class T >
-BiList< T >* create_list()
+template < class T > BiList< T > *create_list()
 {
-  BiList< T >* head = new BiList< T >(); // T::T()
+  BiList< T > *head = new BiList< T >(); // T::T()
   head->next = head;
   head->prev = head;
   return head;
 }
 
-template < class T, class D >
-BiList<T>* insert(BiList<T>* head, T&& val)
+template < class T, class D > BiList< T > *insert(BiList< T > *head, D &&val)
 {
   if (!head) {
     return nullptr;
   }
-  BiList<T>* node = new BiList<T>{std::forward< D >(val), head->next, head}; // T::T(const T&), T::T(T&&)
+  BiList< T > *node = new BiList< T >{std::forward< D >(val), head->next, head}; // T::T(const T&), T::T(T&&)
   head->next->prev = node;
   head->next = node;
   return head;
 }
 
-template < class T, class D >
-BiList< T >* add(BiList< T >* head, T&& val)
+template < class T, class D > BiList< T > *add(BiList< T > *head, D &&val)
 {
   if (!head) {
     return nullptr;
   }
-  BiList< T >* node = new BiList< T >{std::forward< D >(val), head, head->prev}; // T::T(const T&), T::T(T&&)
+  BiList< T > *node = new BiList< T >{std::forward< D >(val), head, head->prev}; // T::T(const T&), T::T(T&&)
   head->prev->next = node;
   head->prev = node;
   return head;
 }
 
-template < class T >
-void pop_front(BiList< T >* head) noexcept
+template < class T > void pop_front(BiList< T > *head) noexcept
 {
   if (!head || head->next == head) {
     return;
   }
-  BiList< T >* temp = head->next;
+  BiList< T > *temp = head->next;
   head->next = temp->next;
   temp->next->prev = head;
   delete temp;
 }
 
-template< class T >
-void clear(BiList< T >* head) noexcept
+template < class T > void clear(BiList< T > *head) noexcept
 {
   if (!head) {
     return;
@@ -70,13 +64,12 @@ void clear(BiList< T >* head) noexcept
   delete head;
 }
 
-template< class T, class F >
-F traverse(BiList< T >* head, F f)
+template < class T, class F > F traverse(BiList< T > *head, F f)
 {
   if (!head) {
     return f;
   }
-  BiList< T >* cur = head->next;
+  BiList< T > *cur = head->next;
   while (cur != head) {
     f(cur->val); // F::operator()
     cur = cur->next;
@@ -84,13 +77,12 @@ F traverse(BiList< T >* head, F f)
   return f; // F::F(const F&)
 }
 
-template< class T >
-BiList< T >* convert(T* arr, size_t s)
+template < class T > BiList< T > *convert(T *arr, size_t s)
 {
   if (!arr) {
     return nullptr;
   }
-  BiList< T >* head = create_list< T >();
+  BiList< T > *head = create_list< T >();
   for (size_t i = 0; i < s; i++) {
     try {
       insert(head, arr[i]); // T::T(const T&), T::T(T&&)
@@ -99,6 +91,6 @@ BiList< T >* convert(T* arr, size_t s)
       throw;
     }
   }
-  delete[] arr; 
+  delete[] arr;
   return head;
 }
